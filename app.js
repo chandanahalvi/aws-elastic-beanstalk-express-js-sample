6fe84d851ef4a72410f76abde9ed18e31e23756a
Zubin Talib <tech@izooky.com>
2021-04-05 13:15:28 -0500

    COMMIT - FULL PIPELINE TEST

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Zubin Talib.'));
+app.get('/', (req, res) => res.send('Hello World! THIS TIME IT\'S GORGON THE DESTROYER!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
